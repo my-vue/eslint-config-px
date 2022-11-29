542d64052fde75bcacd6fd7312e6cbacb011f4fc
memory <zhangzhigang@pxsemic.com>
2022-11-29 15:30:27 +0800

    add globals

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
     root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        defineOptions: 'readonly',
+        NodeJS: 'readonly',
+    },
     extends: [
         'plugin:vue/vue3-essential',
         'eslint:recommended',
